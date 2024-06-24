7c0597909ef0ad218fb69d61c3007d472bf57c55
bartoszfutera <bartosz.futera@gmail.com>
2024-06-24 23:17:44 +0200

    add style

--- twojalistalek/src/components/userform/userform.js
+++ twojalistalek/src/components/userform/userform.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import "./userform.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPills } from '@fortawesome/free-solid-svg-icons'
+import { faPrescription } from '@fortawesome/free-solid-svg-icons'
 
 const Userform = () => {
     return (
         <main className="main container">
-
+            <FontAwesomeIcon icon={faPrescription} className="logo" size="5x"  />
                 <div className="form">
-                    <FontAwesomeIcon icon={faPills} className="form__title" />
                     <h2 className="form__title">Twoja Lista Leków</h2>
                     <p className="form__description">Uzupełnij podane pola</p>
                     <form>
